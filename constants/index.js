8d594b1d38a24c6be317ded6ec54c608c717b2c7
kk786 <techkhalid19@gmail.com>
2026-02-01 21:52:45 +0530

    added Menu

--- constants/index.js
+++ constants/index.js
@@ -134,7 +134,7 @@ const socials = [
     },
 ];
 
-const sliderLists = [
+const cocktailLists2 = [
     {
         id: 1,
         name: "Classic Mojito",
@@ -179,5 +179,5 @@ export {
     openingHours,
     storeInfo,
     socials,
-    sliderLists,
+    cocktailLists2,
 };
